9e92354f0d631c6da6a7a3a45b94815ea5120ec2
Jawad <94877975+Jawad-git@users.noreply.github.com>
2022-12-09 15:20:18 +0200

    make the game loop five times instead showing results

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function getComputerChoice(){
     let x= Math.random();
-    let m;
     if (x <= 0.33) {
         return "rock";
     } 
@@ -10,25 +9,58 @@ function getComputerChoice(){
             else {
                 return "scissors";
             } }
+// function getComputerChoice()
+// {
+//     let x = Math.floor(Math.random() * 3);
+//     let choices = ["rock", "paper", "scissors"];
+//     return choices[x];
+// }
 
 function playerSelection(){
-    console.log("enter rock, paper, or scissors");
     let m = prompt("choose: rock, paper, or scissors");
     return m.toLowerCase();
 }
 // control shift l edit uniform word
 
-function playGame (n, m) {
+function playRound (n, m) {
     if (n == m) {   
-        return(`its a tie! try again.`);
+        return "draw";
     }
-    else if ((n == "scissors" && m == "paper") || (n=="paper" && m=="rock") || (n == "rock" && m == "scissors")) {
-        return( `You win!${n} beats ${m}!`); 
+    else if ((n == "scissors" && m == "paper") || (n=="paper" && m=="rock") ||
+     (n == "rock" && m == "scissors")) {
+        return "win";
     }
         else {
-            return(`you lose, ${m} beats ${n}!`) 
+            return "loss";
         }
 }
 
-let rock, scissors, paper;
-console.log(playGame(playerSelection(), getComputerChoice()));
+function Game(){
+    let myscore=0, pcscore=0, roundresult;
+    for (let i=0; i<5; i++) {
+        roundresult = playRound(playerSelection(), getComputerChoice()); 
+        if (roundresult == "draw") {
+            console.log(`its a draw. the score remains: you ${myscore} - ${pcscore} computer`);
+    }
+        if (roundresult == "win") {
+            myscore++;
+            console.log(`its a Victory! the score changes to: you ${myscore} - ${pcscore} computer`);
+    }
+        if (roundresult == "loss") {
+            console.log(`its a loss.. the score changes to: you ${myscore} - ${++pcscore} computer`);
+    }
+
+
+}
+    if (myscore > pcscore) {
+        console.log("Game completed, you win!")
+    }
+        else if (myscore < pcscore) {
+            console.log("Unfortunately you lose, game over")
+        }
+            else {
+                console.log("Would you look at that! its a tie!")
+            }
+}
+// let myscore, pcscore
+Game();
